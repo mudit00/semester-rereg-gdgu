af576e66c0a5da1a8b5591ece1d60316c39141e8
SwG Ghosh <codecrafts.cf@icloud.com>
2018-03-15 20:22:19 +0530

    Added files for file structure and added db.php for connection

---
+++ nftproject/db.php
@@ -0,0 +1,11 @@
+<?php
+$database_username = "root";
+$database_password = "smahi3003";
+$database_host = "127.0.0.1";
+$database_name = "REGISTRATION";
+
+$database_link = mysqli_connect($database_host, $database_username, $database_password, $database_name);
+
+if($database_link == false) {
+    die("Connection to database could not be established.");
+}
